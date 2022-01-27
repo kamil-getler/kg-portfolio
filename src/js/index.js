57202ddfa6cb20e4139fa37bd73d483dc6e9e08d
KamilGetler <kamilgetler@gmail.com>
2022-01-28 00:00:51 +0100

    NewStyle

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,20 @@ import '../scss/main.scss';
 
 
 
+
+
+
+const kontakt =document.getElementById("kontakt") 
+const projekt =document.getElementById("projekt") 
+const box2 =document.getElementById("click2") 
+const box3 =document.getElementById("click5") 
+
+kontakt.addEventListener('click', () => {
+    box2.classList.toggle('new')
+})
+
+
+projekt.addEventListener('click', () => {
+
+box3.classList.toggle('new')
+})
